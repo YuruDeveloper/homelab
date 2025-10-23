f61ad0f4acdfd8a000f64163871f4346b3ff7c49
YuruDeveloper <vkgkvk8@gmail.com>
2025-10-23 17:38:24 +0900

    fix

--- modules/lxc-ssh-setup/main.tf
+++ modules/lxc-ssh-setup/main.tf
@@ -8,9 +8,9 @@ resource "null_resource" "SshSetup" {
       ssh ${var.ProxmoxUserName}@${var.ProxmoxUrl} "pct exec ${var.VmId} -- apk add --no-cache openssh python3"
       ssh ${var.ProxmoxUserName}@${var.ProxmoxUrl} "pct exec ${var.VmId} -- rc-update add sshd"
       ssh ${var.ProxmoxUserName}@${var.ProxmoxUrl} "pct exec ${var.VmId} -- rc-service sshd start"
-      ssh ${var.ProxmoxUserName}@${var.ProxmoxUrl} "pct exec ${var.VmId} -- sed -i '\$a PermitRootLogin yes' /etc/ssh/sshd_config"
-      ssh ${var.ProxmoxUserName}@${var.ProxmoxUrl} "pct exec ${var.VmId} -- sed -i '\$a PasswordAuthentication no' /etc/ssh/sshd_config"
-      ssh ${var.ProxmoxUserName}@${var.ProxmoxUrl} "pct exec ${var.VmId} -- sed -i '\$a PubkeyAuthentication yes' /etc/ssh/sshd_config"
+      ssh ${var.ProxmoxUserName}@${var.ProxmoxUrl} "pct exec ${var.VmId} -- sh -c \"sed -i 's/^#*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config && if ! grep -q '^PermitRootLogin' /etc/ssh/sshd_config; then echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config; fi\""
+      ssh ${var.ProxmoxUserName}@${var.ProxmoxUrl} "pct exec ${var.VmId} -- sh -c \"sed -i 's/^#*PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config && if ! grep -q '^PasswordAuthentication' /etc/ssh/sshd_config; then echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config; fi\""
+      ssh ${var.ProxmoxUserName}@${var.ProxmoxUrl} "pct exec ${var.VmId} -- sh -c \"sed -i 's/^#*PubkeyAuthentication.*/PubkeyAuthentication yes/' /etc/ssh/sshd_config && if ! grep -q '^PubkeyAuthentication' /etc/ssh/sshd_config; then echo 'PubkeyAuthentication yes' >> /etc/ssh/sshd_config; fi\""
       ssh ${var.ProxmoxUserName}@${var.ProxmoxUrl} "pct exec ${var.VmId} -- rc-service sshd restart"
     EOF
   }
