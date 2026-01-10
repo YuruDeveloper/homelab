45da1eebd7c02dee5e289c19b3363c10ac512141
YuruDeveloper <vkgkvk8@gmail.com>
2026-01-10 17:16:32 +0900

    open argocd

--- opnsense/firewall.tf
+++ opnsense/firewall.tf
@@ -1106,10 +1106,38 @@ resource "opnsense_firewall_filter" "DmzToJenkins" {
   }
 }
 
+# DMZ → ArgoCD
+resource "opnsense_firewall_filter" "DmzToArgocd" {
+  enabled     = true
+  sequence    = 68
+  description = "Allow DMZ to ArgoCD"
+
+  interface = {
+    interface = ["opt5"]
+  }
+
+  filter = {
+    action      = "pass"
+    quick       = true
+    direction   = "in"
+    ip_protocol = "inet"
+    protocol    = "TCP"
+
+    source = {
+      net = "192.168.5.0/24"
+    }
+
+    destination = {
+      net  = "192.168.2.83/32"
+      port = "80"
+    }
+  }
+}
+
 # DMZ → Block remaining internal networks
 resource "opnsense_firewall_filter" "DmzBlockInternal" {
   enabled     = true
-  sequence    = 68
+  sequence    = 69
   description = "Block DMZ access to remaining internal networks"
 
   interface = {
@@ -1136,7 +1164,7 @@ resource "opnsense_firewall_filter" "DmzBlockInternal" {
 # DMZ → Allow Internet
 resource "opnsense_firewall_filter" "DmzToInternet" {
   enabled     = true
-  sequence    = 69
+  sequence    = 70
   description = "Allow DMZ to Internet"
 
   interface = {
@@ -1163,7 +1191,7 @@ resource "opnsense_firewall_filter" "DmzToInternet" {
 # DMZ IPv6 → Allow Internet
 resource "opnsense_firewall_filter" "DmzToInternetIpv6" {
   enabled     = true
-  sequence    = 70
+  sequence    = 71
   description = "Allow DMZ IPv6 to Internet"
 
   interface = {
@@ -1194,7 +1222,7 @@ resource "opnsense_firewall_filter" "DmzToInternetIpv6" {
 # Wireguard → Allow all to any
 resource "opnsense_firewall_filter" "Wireguard" {
   enabled     = true
-  sequence    = 71
+  sequence    = 72
   description = "Allow Wireguard interface to any"
 
   interface = {
@@ -1221,7 +1249,7 @@ resource "opnsense_firewall_filter" "Wireguard" {
 # Wireguard IPv6 → Allow all to any
 resource "opnsense_firewall_filter" "WireguardIpv6" {
   enabled     = true
-  sequence    = 72
+  sequence    = 73
   description = "Allow Wireguard IPv6 interface to any"
 
   interface = {
@@ -1248,7 +1276,7 @@ resource "opnsense_firewall_filter" "WireguardIpv6" {
 # WAN → Allow Wireguard VPN (UDP 51820)
 resource "opnsense_firewall_filter" "WanAllowWireguard" {
   enabled     = true
-  sequence    = 73
+  sequence    = 74
   description = "Allow WAN to Wireguard VPN (UDP 51820)"
 
   interface = {
@@ -1276,7 +1304,7 @@ resource "opnsense_firewall_filter" "WanAllowWireguard" {
 # WAN → Allow Kafka (TCP 9092)
 resource "opnsense_firewall_filter" "WanAllowKafka" {
   enabled     = true
-  sequence    = 74
+  sequence    = 75
   description = "Allow WAN to Kafka (TCP 9092)"
 
   interface = {
