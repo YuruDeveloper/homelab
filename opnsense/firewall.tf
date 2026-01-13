dba30f94592bd24d1f409b19a9e37763e8441baf
YuruDeveloper <vkgkvk8@gmail.com>
2026-01-13 13:45:15 +0900

    add code server

--- opnsense/firewall.tf
+++ opnsense/firewall.tf
@@ -1078,10 +1078,38 @@ resource "opnsense_firewall_filter" "DmzToConvertx" {
   }
 }
 
+# DMZ → Code Server
+resource "opnsense_firewall_filter" "DmzToCodeServer" {
+  enabled     = true
+  sequence    = 67
+  description = "Allow DMZ to Code Server"
+
+  interface = {
+    interface = ["opt5"]
+  }
+
+  filter = {
+    action      = "pass"
+    quick       = true
+    direction   = "in"
+    ip_protocol = "inet"
+    protocol    = "TCP"
+
+    source = {
+      net = "192.168.5.0/24"
+    }
+
+    destination = {
+      net  = "192.168.4.5/32"
+      port = "7070"
+    }
+  }
+}
+
 # DMZ → Jenkins
 resource "opnsense_firewall_filter" "DmzToJenkins" {
   enabled     = true
-  sequence    = 67
+  sequence    = 68
   description = "Allow DMZ to Jenkins"
 
   interface = {
@@ -1109,7 +1137,7 @@ resource "opnsense_firewall_filter" "DmzToJenkins" {
 # DMZ → ArgoCD
 resource "opnsense_firewall_filter" "DmzToArgocd" {
   enabled     = true
-  sequence    = 68
+  sequence    = 69
   description = "Allow DMZ to ArgoCD"
 
   interface = {
@@ -1137,7 +1165,7 @@ resource "opnsense_firewall_filter" "DmzToArgocd" {
 # DMZ → Block remaining internal networks
 resource "opnsense_firewall_filter" "DmzBlockInternal" {
   enabled     = true
-  sequence    = 69
+  sequence    = 70
   description = "Block DMZ access to remaining internal networks"
 
   interface = {
@@ -1164,7 +1192,7 @@ resource "opnsense_firewall_filter" "DmzBlockInternal" {
 # DMZ → Allow Internet
 resource "opnsense_firewall_filter" "DmzToInternet" {
   enabled     = true
-  sequence    = 70
+  sequence    = 71
   description = "Allow DMZ to Internet"
 
   interface = {
@@ -1191,7 +1219,7 @@ resource "opnsense_firewall_filter" "DmzToInternet" {
 # DMZ IPv6 → Allow Internet
 resource "opnsense_firewall_filter" "DmzToInternetIpv6" {
   enabled     = true
-  sequence    = 71
+  sequence    = 72
   description = "Allow DMZ IPv6 to Internet"
 
   interface = {
@@ -1222,7 +1250,7 @@ resource "opnsense_firewall_filter" "DmzToInternetIpv6" {
 # Wireguard → Allow all to any
 resource "opnsense_firewall_filter" "Wireguard" {
   enabled     = true
-  sequence    = 72
+  sequence    = 73
   description = "Allow Wireguard interface to any"
 
   interface = {
@@ -1249,7 +1277,7 @@ resource "opnsense_firewall_filter" "Wireguard" {
 # Wireguard IPv6 → Allow all to any
 resource "opnsense_firewall_filter" "WireguardIpv6" {
   enabled     = true
-  sequence    = 73
+  sequence    = 74
   description = "Allow Wireguard IPv6 interface to any"
 
   interface = {
@@ -1276,7 +1304,7 @@ resource "opnsense_firewall_filter" "WireguardIpv6" {
 # WAN → Allow Wireguard VPN (UDP 51820)
 resource "opnsense_firewall_filter" "WanAllowWireguard" {
   enabled     = true
-  sequence    = 74
+  sequence    = 75
   description = "Allow WAN to Wireguard VPN (UDP 51820)"
 
   interface = {
@@ -1304,7 +1332,7 @@ resource "opnsense_firewall_filter" "WanAllowWireguard" {
 # WAN → Allow Kafka (TCP 9092)
 resource "opnsense_firewall_filter" "WanAllowKafka" {
   enabled     = true
-  sequence    = 75
+  sequence    = 76
   description = "Allow WAN to Kafka (TCP 9092)"
 
   interface = {
